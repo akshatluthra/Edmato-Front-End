cf9bda02b33e30164607c971fbdbbc9c086b542f
akshat luthra <akshatluthra981@gmail.com>
2020-10-07 22:28:21 +0530

    new project

---
+++ build/precache-manifest.c996c45ab55d70adc95ff6b3a52d715e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fcc8e1344663b9786e756f1d240a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ce288c2a02195c5780",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "84e943329eaae87b6fbd",
+    "url": "/static/css/main.77a988f3.chunk.css"
+  },
+  {
+    "revision": "91ce288c2a02195c5780",
+    "url": "/static/js/2.a58d290d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a58d290d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e943329eaae87b6fbd",
+    "url": "/static/js/main.945e96c3.chunk.js"
+  },
+  {
+    "revision": "e0b448b449879f2962c9",
+    "url": "/static/js/runtime-main.5c72e3e1.js"
+  }
+]);
